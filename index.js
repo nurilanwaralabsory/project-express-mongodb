b8fee41c381188075a8a355481ef080113986ea1
nurilanwaralabsory <nurilanwaralabsory@gmail.com>
2024-05-22 16:25:44 +0700

    membuat halaman index dan detail product

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
+const Product = require("./models/product");
 const app = express();
 
 mongoose
@@ -17,6 +18,15 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+app.get("/products", async (req, res) => {
+  const products = await Product.find({});
+  res.render("products/index", { products });
+});
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/show", { product });
 });
 
 app.listen(3000, () => {
